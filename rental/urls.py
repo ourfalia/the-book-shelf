19865d2db46667ccb550518b7a4fd8ff9fd0854e
ourfalia <>
2023-07-22 18:00:29 +0000

    add book search views, urls, update book-list template and css

--- rental/urls.py
+++ rental/urls.py
@@ -6,4 +6,5 @@ from . import views
 
 urlpatterns = [
     path('book_list', views.book_list, name='book_list'),
+    path('search/', views.book_search, name='book_search'),
 ]
--- rental/views.py
+++ rental/views.py
@@ -7,3 +7,12 @@ from .models import Book
 def book_list(request):
     books = Book.objects.all()
     return render(request, 'rental/book_list.html', {'books': books})
+
+
+def book_search(request):
+    query = request.GET.get('q')
+    if query:
+        books = Book.objects.filter(title__icontains=query)
+    else:
+        books = Book.objects.all()
+    return render(request, 'rental/book_list.html', {'books': books})
