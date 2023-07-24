aea5701fe6ee7fd217f07b5f00c9a35ea7def4dd
ourfalia <>
2023-07-24 13:29:13 +0000

    deployment

--- book_rental/settings.py
+++ book_rental/settings.py
@@ -7,7 +7,7 @@ def get_heroku_hostname():
     return os.environ.get('HOSTNAME')
 
 
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
 SECRET_KEY = os.environ.get('SECRET_KEY', '')
