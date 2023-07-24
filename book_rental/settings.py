cbbfe016180c04f82ea1f722bf7f7ec841993c5a
ourfalia <>
2023-07-24 13:16:10 +0000

    deployment

--- book_rental/settings.py
+++ book_rental/settings.py
@@ -103,10 +103,6 @@ else:
     }
 
 
-
-# Password validation
-# https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
-
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
